609716e663bc72f8bfdf74ed18e51b1a8298e8fa
fabiowendel <lfabiowendel@gmail.com>
2021-04-09 00:50:52 -0400

    creando migração das tabelas com BD

---
+++ src/database/migrations/20210409041716-create-project.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('projects', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dateInit: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      dateFinish: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      valueProject: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      riskProject: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('projects');
+  },
+};
